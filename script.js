7d5eafc52cad107b388170c8ac44405ceef05672
Kakha <kakhagiorgashvili@gmail.com>
2020-05-27 14:41:35 +0000

    code beautifier

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
- $(document).ready(function(){
+$(document).ready(function() {
     $('.story').owlCarousel({
-      center: false,
-      items:3,
-      loop:true,
-      margin:5,
-      stagePadding: 15,
-  });
+        center: false,
+        items: 3,
+        loop: true,
+        margin: 5,
+        stagePadding: 15,
+    });
 });
